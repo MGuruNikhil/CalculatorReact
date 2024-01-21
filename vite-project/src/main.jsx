47dde28073e8a515b22c0e4ca9ad58c8a3369f8c
MGuruNikhil <gurunikhilmangaraju@gmail.com>
2024-01-21 11:07:48 +0530

    cleaned

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Calc from './Calc'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Calc />
   </React.StrictMode>,
 )
